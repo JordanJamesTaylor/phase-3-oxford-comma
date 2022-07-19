337e6c7d48b7d28e6f39ea7b1806d298c32b8df7
jordan <jordjamestaylor@gmail.com>
2022-07-18 20:51:24 -0400

    commit tree 7e34da72000952c58a16737172ee9ced6d5a7b15

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+    array.each_with_index.map do |person, index|
+        if index == array.length - 1
+            person.join(", and ")
+        else
+            person.join(", ")
+        end
 end
